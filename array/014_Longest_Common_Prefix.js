1a2c18bdb74191b7aaf6f52e80535488dfd81e88
ruilinqi <ruilinqi7@gmail.com>
2023-03-18 23:24:24 -0400

    Update answer of Longest Common Prefix Array

--- array/014_Longest_Common_Prefix.js
+++ array/014_Longest_Common_Prefix.js
@@ -22,3 +22,25 @@ Constraints:
 0 <= strs[i].length <= 200
 strs[i] consists of only lowercase English letters.
  */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (!strs.length) {
+  return "";
+}
+
+let prefix = strs[0];
+
+for (let i = 1; i < strs.length; i++) {
+  while (strs[i].indexOf(prefix) !== 0) {
+    prefix = prefix.slice(0, prefix.length - 1);
+    if (!prefix) {
+      return "";
+    }
+  }
+}
+
+return prefix;
+};
