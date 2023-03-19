2aeee70c5d0f9f178b75918cc4be4a0809d8f140
ruilinqi <ruilinqi7@gmail.com>
2023-03-18 23:33:45 -0400

    Update answer of Longest Common Prefix Array

--- array/014_Longest_Common_Prefix.js
+++ array/014_Longest_Common_Prefix.js
@@ -27,15 +27,20 @@ strs[i] consists of only lowercase English letters.
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+  // If the strs array is empty, return an empty string
   if (!strs.length) {
   return "";
 }
 
+// Initialize the variable prefix with the first string in the array strs[0]
 let prefix = strs[0];
 
+// Loop through the rest of the strings in the array starting from the second string
 for (let i = 1; i < strs.length; i++) {
+  // While the current string strs[i] does not start with the prefix,  
   while (strs[i].indexOf(prefix) !== 0) {
-    prefix = prefix.slice(0, prefix.length - 1);
+    prefix = prefix.slice(0, prefix.length - 1); // Remove the last character from the prefix, until it is a prefix of the current string or prefix becomes an empty string ""
+    // If prefix becomes an empty string, return an empty string
     if (!prefix) {
       return "";
     }
@@ -44,3 +49,4 @@ for (let i = 1; i < strs.length; i++) {
 
 return prefix;
 };
+// Note: indexOf method is used to check if the prefix is a prefix of the current string
