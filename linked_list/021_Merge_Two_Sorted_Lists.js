5c45309870ae9bb15dd50bfbc759fb1f07bacb4e
ruilinqi <ruilinqi7@gmail.com>
2023-03-20 23:53:47 -0400

    Update answer of linked list - Merge Two Sorted Lists

--- linked_list/021_Merge_Two_Sorted_Lists.js
+++ linked_list/021_Merge_Two_Sorted_Lists.js
@@ -1,4 +1,5 @@
 /**
+ * Question:
  * Write the answer of the question with javascipt: You are given the heads of two sorted linked lists list1 and list2.
 
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -28,3 +29,37 @@ The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.
  */
+
+
+/**
+ * Answer:
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  let dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (list1 && list2) {
+      if (list1.val <= list2.val) {
+          current.next = list1;
+          list1 = list1.next;
+      } else {
+          current.next = list2;
+          list2 = list2.next;
+      }
+      current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return dummy.next;
+};
