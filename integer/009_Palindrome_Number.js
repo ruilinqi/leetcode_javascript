2e0cdf9a88e6b47d103be758a0fa0a139b42e8d1
ruilinqi <ruilinqi7@gmail.com>
2023-03-22 23:44:43 -0400

    Update answer - Palindrome Number

--- integer/009_Palindrome_Number.js
+++ integer/009_Palindrome_Number.js
@@ -26,3 +26,22 @@ Constraints:
 
 -231 <= x <= 231 - 1
  */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  const str = x.toString();
+
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+      if (str.charAt(left) !== str.charAt(right)) {
+          return false;
+      }
+      left++;
+      right--;
+  }
+  return true;
+};
