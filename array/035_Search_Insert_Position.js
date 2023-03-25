229da384dfa2292a15f58280a02e7350f0f62c35
ruilinqi <ruilinqi7@gmail.com>
2023-03-24 23:33:30 -0400

    Update answer - Search Insert Position

--- array/035_Search_Insert_Position.js
+++ array/035_Search_Insert_Position.js
@@ -19,3 +19,24 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4
 
  */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
