a1c0e1ce2ecf92ca497a007f82cf00f918f6f233
ruilinqi <ruilinqi7@gmail.com>
2023-04-12 00:35:03 -0400

    Add question of setTimeout

---
+++ asyncronous/2621_Sleep.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Write an asyncronous function that accepts a positive integer millis and sleeps for that many 
+ * milliseconds. It can resolve any value.
+
+Example 1:
+
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+
+
+Example 2:
+
+Input: millis = 200
+Output: 200
+Explanation: It should return a promise that resolves after 200ms.
+ */
