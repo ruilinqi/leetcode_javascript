f34a5a28e9fb27139dfd565d4aabbaa68f8eec45
ruilinqi <ruilinqi7@gmail.com>
2023-04-11 23:47:31 -0400

    Add answer of couter

--- array/2620_Counter.js
+++ array/2620_Counter.js
@@ -24,3 +24,22 @@ Output: [-2,-1,0,1,2]
 
 Explanation: counter() initially returns -2. Then increases after each sebsequent call.
  */
+
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+  let count = n;
+  return function() {
+      return count++;
+  };
+};
+
+/** 
+* const counter = createCounter(10)
+* counter() // 10
+* counter() // 11
+* counter() // 12
+*/
