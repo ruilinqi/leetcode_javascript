43f94a591b23a4c900834fdd8d9bc04337435b39
ruilinqi <ruilinqi7@gmail.com>
2023-04-13 23:30:46 -0400

    Add answer of javascript array prototype last

--- array/2619_Array_Prototype_Last.js
+++ array/2619_Array_Prototype_Last.js
@@ -14,3 +14,16 @@ Input: nums = []
 Output: -1
 Explanation: Because there are no elements, return -1.
  */
+
+
+Array.prototype.last = function() {
+    if (this.length <= 0) {
+        return -1;
+    }
+    return this[this.length-1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
