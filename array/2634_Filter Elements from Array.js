238b1456d66635674b014e3af8607b9ac80caf31
ruilinqi <ruilinqi7@gmail.com>
2023-04-19 23:09:58 -0700

    Update answer of Filter Elements from Array

--- array/2634_Filter Elements from Array.js
+++ array/2634_Filter Elements from Array.js
@@ -15,6 +15,7 @@ Output: [20,30]
 Explanation:
 const newArray = filter(arr, fn); // [20, 30]
 The function filters out values that are not greater than 10
+
 Example 2:
 
 Input: arr = [1,2,3], fn = function firstIndex(n, i) { return i === 0; }
@@ -22,6 +23,7 @@ Output: [1]
 Explanation:
 fn can also accept the index of each element
 In this case, the function removes elements not at index 0
+
 Example 3:
 
 Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
@@ -47,3 +49,8 @@ var filter = function(arr, fn) {
   }
   return result;
 };
+
+// Use the function
+const arr = [0, 10, 20, 30];
+const fn = function greaterThan10(n) { return n > 10; }
+const newArray = filter(arr, fn); // [20, 30]
