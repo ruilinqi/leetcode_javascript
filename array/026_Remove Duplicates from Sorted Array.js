cd191f01ec1fe08f632be11e999e0d8bc89ba720
ruilinqi <ruilinqi7@gmail.com>
2023-04-25 22:54:17 -0400

    Add more solutions of Remove Duplicates from Sorted Array

--- array/026_Remove Duplicates from Sorted Array.js
+++ array/026_Remove Duplicates from Sorted Array.js
@@ -50,3 +50,40 @@ const removeDuplicates = (nums) => {
         }
     }
 };
+
+// Solution 2 : For loop with continue
+const removeDuplicates = (nums) => {
+    if (nums.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] === nums[i]) continue; // If the elements are equal then continue else copy the unique element by then to "i+1" position
+        nums[++i] = nums[j];
+    }
+    return i + 1;
+}
+
+// Solution 3 : For loop 
+const removeDuplicates = (nums) => {
+    if (nums.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) { //  If the elements are not equal then copy the unique element by then to "i+1" position
+            nums[++i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+
+// Solution 4 :  While loop
+const removeDuplicates = (nums) => {
+    if (nums.length === 0) return 0;
+    let i = 0;
+    let j = 1;
+    while (j < nums.length) {
+        if (nums[j] !== nums[i]) {
+            nums[++i] = nums[j];
+        }
+        j++;
+    }
+    return i + 1;
+}
